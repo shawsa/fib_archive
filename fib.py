81bd9fe78d53c7160788e699fa992726dd2fb23f
Sage <sage.b.shaw@gmail.com>
2023-01-14 18:52:42 -0700

    Wrote some notes on the implementations.

--- fib.py
+++ fib.py
@@ -1,29 +1,49 @@
+from itertools import islice
+from more_itertools import iterate, nth
+from functools import partial
 
+def test10(foo):
+    return list(map(foo, range(10)))
 
-def test(foo):
-    print(list(map(foo, range(10))))
+implementations = {}
+def register(foo):
+    implementations[foo.__name__] = foo
+    return foo
 
-def fib(n: int) -> int:
+@register
+def fib_naive(n: int) -> int:
     if n < 2:
-        return 1
-    return fib(n-1) + fib(n-2) 
+        return n
+    return fib_naive(n-1) + fib_naive(n-2) 
 
-test(fib)
-
+@register
 def fib_lin(n: int) -> int:
     if n < 2:
-        return 1
-    a, b = 1, 1
+        return n
+    a, b = 0, 1
     for _ in range(n-1):
         a, b = b, a + b
     return b
 
-test(fib_lin)
+@register
+def fib_lin_functional(n: int):
+    def fib_iteration(tup):
+        a, b = tup
+        return b, a+b
 
+    second = partial(nth, n=1)
 
+    def fib_seq():
+        yield 0
+        yield from map(second,
+                       iterate(fib_iteration, (0, 1)))
+
+    return nth(fib_seq(), n)
+
+@register
 def fib_log(n: int) -> int:
     if n < 2:
-        return 1
+        return n
 
     def matmul(m1, m2):
         return (
@@ -32,14 +52,23 @@ def fib_log(n: int) -> int:
                 m1[2]*m2[0] + m1[3]*m2[2],
                 m1[2]*m2[1] + m1[3]*m2[3]
                 )
-    mat = (1, 0, 0, 1)
+
+    def mat_vec_mul(m, v):
+        return (m[0]*v[0] + m[1]*v[1], m[2]*v[0] + m[3]*v[1])
+
+    vec = (1, 0)
     powmat = (1, 1, 1, 0)
+    n -= 1  # one fewer steps to take than terms in the sequence.
     while n > 0:
         if n%2 == 1:
-            mat = matmul(mat, powmat)
+            vec = mat_vec_mul(powmat, vec)
         powmat = matmul(powmat, powmat)
         n //= 2
 
-    return mat[0]
+    return vec[0]
 
-test(fib_log)
+
+if __name__ == '__main__':
+    max_len = max(map(len, implementations.keys()))
+    for name, foo in implementations.items():
+        print(f'{foo.__name__.rjust(max_len)}: {test10(foo)}')
